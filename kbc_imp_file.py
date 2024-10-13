6f41c14f8384e45936291eb0d5b76fa092995095
sh1nch4n-Nohara <moto.g34.24.05.2024@gmail.com>
2024-10-13 23:31:58 +0530

    Create kbc_imp_file.py
    add here more questions and anwers... you can freely edit here inside []s and dont forget to add comma and quotation

---
+++ kbc_imp_file.py
@@ -0,0 +1,6 @@
+q=['what is the capital of india?','who is the pm of india?','who is the owner of this code?']
+a=['delhi','narendra modi','momin']
+
+''' q is for questions and a is for answers , you can edit here, add more questions and ans 
+but plz add them in the same index
+e.g. 3rd question's ans is 3rd element in a list'''
